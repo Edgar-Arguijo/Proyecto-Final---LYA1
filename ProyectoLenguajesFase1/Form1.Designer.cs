27d67ab29eb09e89ee98a942916d9d8eea1e4d29
Edgar Arguijo <70034525+Edgar-Arguijo@users.noreply.github.com>
2022-06-14 19:47:20 -0500

    Agregar archivos de proyecto.

---
+++ ProyectoLenguajesFase1/Form1.Designer.cs
@@ -0,0 +1,307 @@
+﻿namespace ProyectoLenguajesFase1
+{
+    partial class FormProyecto
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.archivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.abrir = new System.Windows.Forms.ToolStripMenuItem();
+            this.nuevoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.guardarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.guardaHTMLToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.salirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.txtEntrada = new System.Windows.Forms.RichTextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.txtSalida = new System.Windows.Forms.RichTextBox();
+            this.dataLexemas = new System.Windows.Forms.DataGridView();
+            this.Lexema = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Token = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataErrores = new System.Windows.Forms.DataGridView();
+            this.Error = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Token2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tbngenerar = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataLexemas)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataErrores)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.archivoToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(4, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(937, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // archivoToolStripMenuItem
+            // 
+            this.archivoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.abrir,
+            this.nuevoToolStripMenuItem,
+            this.guardarToolStripMenuItem,
+            this.guardaHTMLToolStripMenuItem,
+            this.salirToolStripMenuItem});
+            this.archivoToolStripMenuItem.Name = "archivoToolStripMenuItem";
+            this.archivoToolStripMenuItem.Size = new System.Drawing.Size(60, 20);
+            this.archivoToolStripMenuItem.Text = "Archivo";
+            // 
+            // abrir
+            // 
+            this.abrir.Name = "abrir";
+            this.abrir.Size = new System.Drawing.Size(139, 22);
+            this.abrir.Text = "Abrir";
+            this.abrir.Click += new System.EventHandler(this.abrirToolStripMenuItem_Click);
+            // 
+            // nuevoToolStripMenuItem
+            // 
+            this.nuevoToolStripMenuItem.Name = "nuevoToolStripMenuItem";
+            this.nuevoToolStripMenuItem.Size = new System.Drawing.Size(139, 22);
+            this.nuevoToolStripMenuItem.Text = "Nuevo";
+            this.nuevoToolStripMenuItem.Click += new System.EventHandler(this.nuevoToolStripMenuItem_Click);
+            // 
+            // guardarToolStripMenuItem
+            // 
+            this.guardarToolStripMenuItem.Name = "guardarToolStripMenuItem";
+            this.guardarToolStripMenuItem.Size = new System.Drawing.Size(139, 22);
+            this.guardarToolStripMenuItem.Text = "Guardar";
+            this.guardarToolStripMenuItem.Click += new System.EventHandler(this.guardarToolStripMenuItem_Click);
+            // 
+            // guardaHTMLToolStripMenuItem
+            // 
+            this.guardaHTMLToolStripMenuItem.Name = "guardaHTMLToolStripMenuItem";
+            this.guardaHTMLToolStripMenuItem.Size = new System.Drawing.Size(139, 22);
+            this.guardaHTMLToolStripMenuItem.Text = "Guarda XML";
+            this.guardaHTMLToolStripMenuItem.Click += new System.EventHandler(this.guardaHTMLToolStripMenuItem_Click);
+            // 
+            // salirToolStripMenuItem
+            // 
+            this.salirToolStripMenuItem.Name = "salirToolStripMenuItem";
+            this.salirToolStripMenuItem.Size = new System.Drawing.Size(139, 22);
+            this.salirToolStripMenuItem.Text = "Salir";
+            this.salirToolStripMenuItem.Click += new System.EventHandler(this.salirToolStripMenuItem_Click);
+            // 
+            // txtEntrada
+            // 
+            this.txtEntrada.Font = new System.Drawing.Font("Arial Black", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEntrada.Location = new System.Drawing.Point(18, 65);
+            this.txtEntrada.Margin = new System.Windows.Forms.Padding(2);
+            this.txtEntrada.Name = "txtEntrada";
+            this.txtEntrada.Size = new System.Drawing.Size(443, 341);
+            this.txtEntrada.TabIndex = 1;
+            this.txtEntrada.Text = "";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // txtSalida
+            // 
+            this.txtSalida.Enabled = false;
+            this.txtSalida.Font = new System.Drawing.Font("Arial Black", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSalida.Location = new System.Drawing.Point(483, 65);
+            this.txtSalida.Margin = new System.Windows.Forms.Padding(2);
+            this.txtSalida.Name = "txtSalida";
+            this.txtSalida.Size = new System.Drawing.Size(443, 262);
+            this.txtSalida.TabIndex = 2;
+            this.txtSalida.Text = "";
+            // 
+            // dataLexemas
+            // 
+            this.dataLexemas.AllowUserToDeleteRows = false;
+            this.dataLexemas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataLexemas.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Lexema,
+            this.Token});
+            this.dataLexemas.Location = new System.Drawing.Point(483, 379);
+            this.dataLexemas.Margin = new System.Windows.Forms.Padding(2);
+            this.dataLexemas.Name = "dataLexemas";
+            this.dataLexemas.ReadOnly = true;
+            this.dataLexemas.RowTemplate.Height = 24;
+            this.dataLexemas.Size = new System.Drawing.Size(428, 227);
+            this.dataLexemas.TabIndex = 5;
+            // 
+            // Lexema
+            // 
+            this.Lexema.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.DisplayedCells;
+            this.Lexema.HeaderText = "Lexema";
+            this.Lexema.Name = "Lexema";
+            this.Lexema.ReadOnly = true;
+            this.Lexema.Width = 69;
+            // 
+            // Token
+            // 
+            this.Token.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.DisplayedCells;
+            this.Token.HeaderText = "Token";
+            this.Token.Name = "Token";
+            this.Token.ReadOnly = true;
+            this.Token.Width = 63;
+            // 
+            // dataErrores
+            // 
+            this.dataErrores.AllowUserToDeleteRows = false;
+            this.dataErrores.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataErrores.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Error,
+            this.Token2});
+            this.dataErrores.Location = new System.Drawing.Point(18, 448);
+            this.dataErrores.Margin = new System.Windows.Forms.Padding(2);
+            this.dataErrores.Name = "dataErrores";
+            this.dataErrores.ReadOnly = true;
+            this.dataErrores.RowTemplate.Height = 24;
+            this.dataErrores.Size = new System.Drawing.Size(428, 209);
+            this.dataErrores.TabIndex = 6;
+            // 
+            // Error
+            // 
+            this.Error.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.DisplayedCells;
+            this.Error.HeaderText = "Error";
+            this.Error.Name = "Error";
+            this.Error.ReadOnly = true;
+            this.Error.Width = 54;
+            // 
+            // Token2
+            // 
+            this.Token2.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.DisplayedCells;
+            this.Token2.HeaderText = "Token";
+            this.Token2.Name = "Token2";
+            this.Token2.ReadOnly = true;
+            this.Token2.Width = 63;
+            // 
+            // tbngenerar
+            // 
+            this.tbngenerar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.tbngenerar.Location = new System.Drawing.Point(483, 622);
+            this.tbngenerar.Margin = new System.Windows.Forms.Padding(2);
+            this.tbngenerar.Name = "tbngenerar";
+            this.tbngenerar.Size = new System.Drawing.Size(428, 36);
+            this.tbngenerar.TabIndex = 9;
+            this.tbngenerar.Text = "Analizar y Generar";
+            this.tbngenerar.UseVisualStyleBackColor = false;
+            this.tbngenerar.Click += new System.EventHandler(this.tbngenerar_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(483, 342);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(194, 33);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Palabras Reservadas";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(18, 414);
+            this.button2.Margin = new System.Windows.Forms.Padding(2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(204, 30);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Error Sintaxtico";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.button3.Enabled = false;
+            this.button3.Location = new System.Drawing.Point(18, 31);
+            this.button3.Margin = new System.Windows.Forms.Padding(2);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(204, 30);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "Archivo de Entrada";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // FormProyecto
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(937, 668);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tbngenerar);
+            this.Controls.Add(this.dataErrores);
+            this.Controls.Add(this.dataLexemas);
+            this.Controls.Add(this.txtSalida);
+            this.Controls.Add(this.txtEntrada);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "FormProyecto";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Proyecto Final";
+            this.Load += new System.EventHandler(this.FormProyecto_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataLexemas)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataErrores)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem archivoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem abrir;
+        private System.Windows.Forms.ToolStripMenuItem guardarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem nuevoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem salirToolStripMenuItem;
+        private System.Windows.Forms.RichTextBox txtEntrada;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.ToolStripMenuItem guardaHTMLToolStripMenuItem;
+        private System.Windows.Forms.RichTextBox txtSalida;
+        private System.Windows.Forms.DataGridView dataLexemas;
+        private System.Windows.Forms.DataGridView dataErrores;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Lexema;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Token;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Error;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Token2;
+        private System.Windows.Forms.Button tbngenerar;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
